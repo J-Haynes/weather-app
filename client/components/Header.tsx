7b414c1907c3e22c615a0c623870774e0cec44b3
Jack Haynes <haynes.jack.stewart@gmail.com>
2023-04-25 20:08:44 +1200

    initial commit

---
+++ client/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="nav">
+      <div className="title-div">
+        <p>weather app</p>
+      </div>
+      <div className="nav-div">
+        <ul>
+          <li>home</li>
+          <li>about</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
---
+++ server/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from 'path'
+import { join } from 'node:path'
+
+const server = express()
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'public')))
+
+server.get('*', (req, res) => {
+  res.sendFile(join(__dirname, './public/index.html'))
+})
+
+export default server
